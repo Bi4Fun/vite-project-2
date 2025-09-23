152760eac3e7774f02face16c4bde5428ad84aa3
DatDoan <doantiendat0502@gmail.com>
2025-09-23 21:28:47 +0700

    cleanup blob after choose avatar avoid memory leak

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import style from "./Admin.module.scss";
 import defaultAvatar from "../../assets/images/default-avatar.png";
+
 function Admin() {
+  const [previewAvatar, setPreviewAvatar] = useState("");
+
+  // Xử lý preview ảnh render lên (dùng blob)
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    const previewURL = URL.createObjectURL(file);
+    setPreviewAvatar(previewURL);
+    console.log(previewURL);
+  }
+  // Dọn dẹp blob khi chọn ảnh khác (sẽ loại bỏ blob ảnh cũ) để tránh rò rỉ bộ nhớ
+  useEffect(() => {
+    return () => {
+      if (previewAvatar) {
+        URL.revokeObjectURL(previewAvatar);
+      }
+    };
+  }, [previewAvatar]);
+
   return (
     <>
       <div className={style.wrapper}>
         Admin page
         <label>
-          <input type="file" hidden />
+          <input type="file" hidden onChange={handleFileChange} />
           <img
             className={style.img_avatar}
-            src={defaultAvatar}
+            src={previewAvatar || defaultAvatar}
             alt="Admin avatar"
           />
         </label>
